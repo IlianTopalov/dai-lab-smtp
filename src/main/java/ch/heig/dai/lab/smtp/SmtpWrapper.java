1b2e7a6fcbfb90281dde67c42df0b7ba1b5f5812
Chomusuke <yukhanov.artur@gmail.com>
2024-12-02 14:47:04 +0100

    Removed comment

--- src/main/java/ch/heig/dai/lab/smtp/SmtpWrapper.java
+++ src/main/java/ch/heig/dai/lab/smtp/SmtpWrapper.java
@@ -5,21 +5,6 @@ public class SmtpWrapper {
 	private SmtpWrapper() {}
 
 	public static String wrap(String from, String to, String content) {
-		/**
-		 *                      EHLO bar.com
-		 *                      MAIL FROM:%s
-		 *                      RCPT TO:%s
-		 *                      DATA
-		 *                      Date: Thu, 21 May 1998 05:33:29 -0700
-		 *                      From: %s
-		 *                      Subject: The Next Meeting of the Board
-		 *                      To: %s
-		 *
-		 *                      %s
-		 *
-		 *                      QUIT
-		 */
-
 		return String.format(
 			"""
 			HELO
