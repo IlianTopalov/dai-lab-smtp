459fba90bec92dd19542f0350636bce00ef72d32
Chomusuke <yukhanov.artur@gmail.com>
2024-12-02 14:46:50 +0100

    Re-indented with tabs

--- src/main/java/ch/heig/dai/lab/smtp/SmtpWrapper.java
+++ src/main/java/ch/heig/dai/lab/smtp/SmtpWrapper.java
@@ -2,37 +2,37 @@ package ch.heig.dai.lab.smtp;
 
 public class SmtpWrapper {
 
-    private SmtpWrapper() {}
+	private SmtpWrapper() {}
 
-    public static String wrap(String from, String to, String content) {
-        /**
-         *                      EHLO bar.com
-         *                      MAIL FROM:%s
-         *                      RCPT TO:%s
-         *                      DATA
-         *                      Date: Thu, 21 May 1998 05:33:29 -0700
-         *                      From: %s
-         *                      Subject: The Next Meeting of the Board
-         *                      To: %s
-         *
-         *                      %s
-         *
-         *                      QUIT
-         */
+	public static String wrap(String from, String to, String content) {
+		/**
+		 *                      EHLO bar.com
+		 *                      MAIL FROM:%s
+		 *                      RCPT TO:%s
+		 *                      DATA
+		 *                      Date: Thu, 21 May 1998 05:33:29 -0700
+		 *                      From: %s
+		 *                      Subject: The Next Meeting of the Board
+		 *                      To: %s
+		 *
+		 *                      %s
+		 *
+		 *                      QUIT
+		 */
 
-        return String.format(
-                     """
-                     HELO
-                     MAIL FROM:%s
-                     RCPT TO:%s
-                     DATA
-                     From: %s
-                     Subject: Troll
-                     To: %s
-                
-                     %s
-                
-                     QUIT
-                """, from, to, from, to, content);
-    }
+		return String.format(
+			"""
+			HELO
+			MAIL FROM:%s
+			RCPT TO:%s
+			DATA
+			From: %s
+			Subject: Troll
+			To: %s
+
+			%s
+
+			QUIT
+			""", from, to, from, to, content);
+	}
 }
