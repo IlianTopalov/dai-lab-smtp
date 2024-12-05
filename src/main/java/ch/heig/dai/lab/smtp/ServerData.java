4e581501e788ac64e085fa76c15b0e4ded49993d
Chomusuke <yukhanov.artur@gmail.com>
2024-12-05 22:27:43 +0100

    Added toString

--- src/main/java/ch/heig/dai/lab/smtp/ServerData.java
+++ src/main/java/ch/heig/dai/lab/smtp/ServerData.java
@@ -1,3 +1,8 @@
 package ch.heig.dai.lab.smtp;
 
-public record ServerData(String host, int port) { }
+public record ServerData(String host, int port) {
+	@Override
+	public String toString() {
+		return host + ":" + port;
+	}
+}
