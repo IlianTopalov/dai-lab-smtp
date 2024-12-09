934efbed48ac74f6cad9d29119daae3f1aa89c20
IlianTopalov <ilian.topalov@heig-vd.ch>
2024-12-09 14:14:09 +0100

    feat: config_file implementation

--- src/main/java/ch/heig/dai/lab/smtp/Main.java
+++ src/main/java/ch/heig/dai/lab/smtp/Main.java
@@ -11,23 +11,51 @@ public class Main {
 	private static final int MIN_ADDRESSES_IN_GROUP = 2;
 	private static final int MAX_ADDRESSES_IN_GROUP = 5;
 
-	private static final String SUBJECT = "Prank";
-
 	public static void main(String[] args) {
-		if (args.length != 3)
-			throw new IllegalArgumentException("Correct command: smtp <addresses file> <messages file> <group count>");
+		if ((args.length == 2 && !args[0].equals("-config")) || args.length == 1 || args.length > 2)
+			throw new IllegalArgumentException("Correct command: smtp -config <config file>");
+
+		List<String> config;
+
+		try {
+			if (args.length == 2) {
+				config = FileUtil.readList(args[0]);
+			} else {
+				config = FileUtil.readList("configuration.config");
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		String h = config.getFirst();
+		final String host = h.substring(h.indexOf(':'), h.length() - 1);
+
+		String p = config.get(1);
+		final int port = Integer.parseInt(p.substring(p.indexOf(':'), p.length() - 1));
+
+		final ServerData SERVER = new ServerData(host, port);
+
+		String addr = config.get(2);
+		final String addr_path = addr.substring(addr.indexOf(':'), addr.length() - 1);
+
+		String msg = config.get(3);
+		final String msg_path = msg.substring(msg.indexOf(':'), msg.length() - 1);
+
+		String s = config.get(4);
+		final String SUBJECT = s.substring(s.indexOf(':'), s.length() - 1);
+
+		String g = config.get(5);
+		final int groupCount = Integer.parseInt(g.substring(g.indexOf(':'), g.length() - 1));
 
 		List<String> addresses;
 		List<String> messages;
 
 		try {
-			addresses = FileUtil.readList(args[0]);
-			messages = FileUtil.readList(args[1]);
+			addresses = FileUtil.readList(addr_path);
+			messages = FileUtil.readList(msg_path);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-
-		int groupCount = Integer.parseInt(args[2]);
 
 		if (addresses.size() < groupCount * MIN_ADDRESSES_IN_GROUP)
 			throw new IllegalArgumentException("Too few addresses.");
