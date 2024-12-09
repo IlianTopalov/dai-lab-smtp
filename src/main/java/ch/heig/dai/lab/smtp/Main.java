72c1afdd659cbef62dcbfe52f3070e573a6dded9
Chomusuke <yukhanov.artur@gmail.com>
2024-12-09 14:39:53 +0100

    Improved config file implementation

--- src/main/java/ch/heig/dai/lab/smtp/Main.java
+++ src/main/java/ch/heig/dai/lab/smtp/Main.java
@@ -1,6 +1,7 @@
 package ch.heig.dai.lab.smtp;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Random;
 
@@ -9,48 +10,43 @@ public class Main {
 	private static final int MIN_ADDRESSES_IN_GROUP = 2;
 	private static final int MAX_ADDRESSES_IN_GROUP = 5;
 
+	private static final String DEFAULT_CONFIG_FILE_NAME = "config";
+
 	public static void main(String[] args) {
 		if ((args.length == 2 && !args[0].equals("-config")) || args.length == 1 || args.length > 2)
 			throw new IllegalArgumentException("Correct command: smtp -config <config file>");
 
-		List<String> config;
+		List<String> addresses;
+		List<String> messages;
 
-		try {
-			if (args.length == 2) {
-				config = FileUtil.readList(args[0]);
-			} else {
-				config = FileUtil.readList("smtpSender.config");
-			}
+		String configFileName = (args.length == 2 ? args[1] : DEFAULT_CONFIG_FILE_NAME);
+
+		final ServerData server;
+		final String addressesPath;
+		final String messagesPath;
+		final String subject;
+		final int groupCount;
+
+		try (BufferedReader configFile = new BufferedReader(new InputStreamReader(
+			new FileInputStream(configFileName),
+			StandardCharsets.UTF_8
+		))) {
+			server = new ServerData(
+				extractColonValue(configFile.readLine()),
+				Integer.parseInt(extractColonValue(configFile.readLine()))
+			);
+
+			addressesPath = extractColonValue(configFile.readLine());
+			messagesPath = extractColonValue(configFile.readLine());
+			subject = extractColonValue(configFile.readLine());
+			groupCount = Integer.parseInt(extractColonValue(configFile.readLine()));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 
-		String h = config.getFirst();
-		final String host = h.substring(h.indexOf(':'), h.length() - 1);
-
-		String p = config.get(1);
-		final int port = Integer.parseInt(p.substring(p.indexOf(':'), p.length() - 1));
-
-		final ServerData SERVER = new ServerData(host, port);
-
-		String addr = config.get(2);
-		final String addr_path = addr.substring(addr.indexOf(':'), addr.length() - 1);
-
-		String msg = config.get(3);
-		final String msg_path = msg.substring(msg.indexOf(':'), msg.length() - 1);
-
-		String s = config.get(4);
-		final String SUBJECT = s.substring(s.indexOf(':'), s.length() - 1);
-
-		String g = config.get(5);
-		final int groupCount = Integer.parseInt(g.substring(g.indexOf(':'), g.length() - 1));
-
-		List<String> addresses;
-		List<String> messages;
-
 		try {
-			addresses = FileUtil.readList(addr_path);
-			messages = FileUtil.readList(msg_path);
+			addresses = FileUtil.readList(addressesPath);
+			messages = FileUtil.readList(messagesPath);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -61,7 +57,7 @@ public class Main {
 			throw new IllegalArgumentException("Too many addresses.");
 
 		List<List<String>> groups = FileUtil.groupLines(addresses, groupCount);
-		try (SMTPSender mailCannon = new SMTPSender(SERVER)) {
+		try (SMTPSender mailCannon = new SMTPSender(server)) {
 			for (List<String> group : groups) {
 				int msgIdx = Math.abs(new Random().nextInt() % messages.size());
 
@@ -71,9 +67,21 @@ public class Main {
 				for (int i = 1; i < group.size(); ++i) {
 					String receiver = group.get(i);
 
-					mailCannon.sendMessage(sender, receiver, SUBJECT, message);
+					mailCannon.sendMessage(sender, receiver, subject, message);
 				}
 			}
 		}
+	}
+
+
+	/**
+	 * Returns the value stored in a String after a colon.
+	 *
+	 * @param line the line from which to extract a value.
+	 *
+	 * @return The value.
+	 */
+	private static String extractColonValue(String line) {
+		return line.split(":")[1].trim();
 	}
 }
